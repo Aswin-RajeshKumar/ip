4e940a1d9a4b9d1073b85538045aebb10cba8f69
Aswin Rajesh Kumar <aswinrajeshb@gmail.com>
2026-02-03 21:59:12 +0800

    Level-3: Mark tasks as done

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -57,25 +57,45 @@ public class Nova {
     }
 }
 
+/**
+ * Represents a task with a description and completion status.
+ */
 class Task {
-    private String description;
+
+    private final String description;
     private boolean isDone;
 
+    /**
+     * Constructs a new Task with the given description.
+     *
+     * @param description Description of the task
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     */
     public void markAsNotDone() {
         isDone = false;
     }
 
+    /**
+     * Returns the status icon of the task.
+     *
+     * @return "X" if done, otherwise a blank space
+     */
     public String getStatusIcon() {
-        return isDone ? "X" : " "; // X for done, blank for not done
+        return isDone ? "X" : " ";
     }
 
     @Override
