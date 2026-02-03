e76a1295a32f38cc78d958421cb28256e8438926
Aswin Rajesh Kumar <aswinrajeshb@gmail.com>
2026-02-03 20:32:00 +0800

    Level-1: Echo

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Nova {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Nova");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Nova");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        String input = "";
+        while (true) {
+            input = sc.nextLine(); // read user input
+            if (input.equals("bye")) {
+                break; // exit loop without echoing
+            }
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + input); // echo the command with proper indentation
+            System.out.println("    ____________________________________________________________");
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+
+        sc.close();
     }
 }
